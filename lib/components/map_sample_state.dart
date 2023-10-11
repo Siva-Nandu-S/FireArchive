e1b9042625e2175d48add72e39a5b8c0f8ffa184
Siva Nandu S <sivanandus2003@gmail.com>
2023-10-11 22:47:30 +0530

    Added locatio to details

--- lib/components/map_sample_state.dart
+++ lib/components/map_sample_state.dart
@@ -13,51 +13,6 @@ import "dart:math" show asin, cos, pi, pow, sin, sqrt;
 import 'package:geocoding/geocoding.dart';
 import 'package:fire_archive/components/alert_button.dart';
 
-class AirQualityBar extends StatelessWidget {
-  final int aqi; // Air Quality Index value
-  final List<Color> gradientColors;
-
-  AirQualityBar({required this.aqi}) : gradientColors = _getGradientColors(aqi);
-
-  static List<Color> _getGradientColors(int aqi) {
-    // Define color ranges based on AQI levels
-    if (aqi >= 0 && aqi <= 1) {
-      // Green: 0 to 50
-      return [Colors.green, Colors.green];
-    } else if (aqi <= 2) {
-      // Yellow: 51 to 100
-      return [Colors.yellow, Colors.yellow];
-    } else if (aqi <= 3) {
-      // Orange: 101 to 150
-      return [Colors.orange, Colors.orange];
-    } else if (aqi <= 4) {
-      // Red: 151 to 200
-      return [Colors.red, Colors.red];
-    }else {
-      // Maroon: 301 and higher
-      return [const Color.fromARGB(255, 128, 0, 0), Color.fromARGB(255, 128, 0, 0),];
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 20.0, // Adjust the height of the bar as needed
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: gradientColors,
-          begin: Alignment.centerLeft,
-          end: Alignment.centerRight,
-        ),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-    );
-  }
-}
-
-
-
-
 class MapSampleState extends State<MapSample> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final Completer<GoogleMapController> _controller =
@@ -106,14 +61,12 @@ class MapSampleState extends State<MapSample> {
 
       setMarkers(hotspots!);
     });
-    
   }
 
   List<Marker> spots = [];
   List<Marker> redSpots = [];
 
   void setMarkers(locations) async {
-
     var data = locations[0].join(',').toString();
     var dataList = data.split('\n');
 
@@ -194,63 +147,94 @@ class MapSampleState extends State<MapSample> {
         .then((response) async {
       data = json.decode(response.body);
     });
+
+    var location = await placemarkFromCoordinates(latitude, longitude);
+    print(location.toString());
+    var fire_location = location[0].locality.toString();
+
     int aqi = data['list'][0]['main']['aqi'];
     data = data['list'][0]['components'];
+
     // ignore: use_build_context_synchronously
 
-
- Widget _buildDetailRow(String title, String value){  // Build the details row.
+    Widget _buildDetailRow(String title, String value) {
+      // Build the details row.
       return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,  
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           Text(title),
           Text(value),
         ],
       );
     }
+
     // ignore: use_build_context_synchronously
     return showDialog<void>(
       context: context,
-      barrierDismissible: true, // user must tap button!
+      barrierDismissible: true, // User can tap outside to close the dialog
       builder: (BuildContext context) {
-    return AlertDialog(
-      title: const Text('Details', style: TextStyle(fontWeight: FontWeight.bold)),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-      content: Container(
-        height: 310,
-        width: 100,
-        child: Column(
-        children: <Widget>[
-          const SizedBox(height: 10),
-          const Text('Air Quality Index', style: TextStyle(fontWeight: FontWeight.bold)),
-          const SizedBox(height: 5),
-          IntensityMeter(intensity: aqi,),
-          const SizedBox(height: 15), 
-          _buildDetailRow('Air Quality', '$aqi'),
-          _buildDetailRow('CO', '${data['co']}'),
-          _buildDetailRow('NO', '${data['no']}'),
-          _buildDetailRow('NO2', '${data['no2']}'),
-          _buildDetailRow('O3', '${data['o3']}'),
-          _buildDetailRow('SO2', '${data['so2']}'),
-          _buildDetailRow('PM2.5', '${data['pm2_5']}'),
-          _buildDetailRow('PM10', '${data['pm10']}'),
-          _buildDetailRow('NH3', '${data['nh3']}'),
-        ],
-      ),
-      ),
-      actions: <Widget>[
-        TextButton(
-          child: Text('Done', style: TextStyle(color: Colors.blue)),
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-        ),
-      ],
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+          ),
+          child: Container(
+            height: 480,
+            width: 300,
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Details',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(height: 10),
+                const Divider(), // Horizontal line for separation
+                SizedBox(height: 10),
+                const Text(
+                  'Air Quality Index',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 5),
+                IntensityMeter(
+                  intensity: aqi,
+                ),
+                SizedBox(height: 15),
+                _buildDetailRow('Location', fire_location),
+                _buildDetailRow('Air Quality', (aqi).toString()),
+                _buildDetailRow('CO', data['co'].toString()),
+                _buildDetailRow('NO', data['no'].toString()),
+                _buildDetailRow('NO2', data['no2'].toString()),
+                _buildDetailRow('O3', data['o3'].toString()),
+                _buildDetailRow('SO2', data['so2'].toString()),
+                _buildDetailRow('PM2.5', data['pm2_5'].toString()),
+                _buildDetailRow('PM10', data['pm10'].toString()),
+                _buildDetailRow('NH3', data['nh3'].toString()),
+                SizedBox(height: 15),
+                Center(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.blue),
+                      foregroundColor: MaterialStateProperty.all(Colors.white),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('Done'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
     );
-  },
-);
   }
 
   double degreesToRadians(double degrees) {
@@ -281,7 +265,6 @@ class MapSampleState extends State<MapSample> {
     }
   }
 
-
   @override
   void dispose() {
     // Dispose of resources here.
@@ -298,47 +281,48 @@ class MapSampleState extends State<MapSample> {
     return await Geolocator.getCurrentPosition();
   }
 
- void searchLocation(String searchedLocation) async {
-  try {
-    List<Location> locations = await locationFromAddress(searchedLocation);
+  void searchLocation(String searchedLocation) async {
+    try {
+      List<Location> locations = await locationFromAddress(searchedLocation);
 
-    if (locations.isNotEmpty) {
-      Location firstLocation = locations.first;
-      double latitude = firstLocation.latitude;
-      double longitude = firstLocation.longitude;
+      if (locations.isNotEmpty) {
+        Location firstLocation = locations.first;
+        double latitude = firstLocation.latitude;
+        double longitude = firstLocation.longitude;
 
-
-      markers.add(
-        Marker(
-          markerId: const MarkerId("searchedLocation"),
-          position: LatLng(latitude, longitude),
-          infoWindow: InfoWindow(
-            title: searchedLocation,
+        markers.add(
+          Marker(
+            markerId: const MarkerId("searchedLocation"),
+            position: LatLng(latitude, longitude),
+            infoWindow: InfoWindow(
+              title: searchedLocation,
+            ),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueViolet),
+            onTap: () {
+              _showMyDialog(latitude, longitude);
+            },
           ),
-          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueViolet),
-          onTap: () {
-            _showMyDialog(latitude, longitude);
-          },
-        ),
-      );
+        );
 
-      // Move the camera to the searched location
-      CameraPosition cameraPosition = CameraPosition(
-        target: LatLng(latitude, longitude),
-        zoom: 8,
-      );
+        // Move the camera to the searched location
+        CameraPosition cameraPosition = CameraPosition(
+          target: LatLng(latitude, longitude),
+          zoom: 8,
+        );
 
-      final GoogleMapController controller = await _controller.future;
-      controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-      setState(() {});
-    } else {
-      // Handle the case where no location data is available
-      print('No location data available for: $searchedLocation');
+        final GoogleMapController controller = await _controller.future;
+        controller
+            .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+        setState(() {});
+      } else {
+        // Handle the case where no location data is available
+        print('No location data available for: $searchedLocation');
+      }
+    } catch (e) {
+      print('Error searching location: $e');
     }
-  } catch (e) {
-    print('Error searching location: $e');
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -353,41 +337,42 @@ class MapSampleState extends State<MapSample> {
     );
   }
 
- AppBar myAppBar() {
-  return AppBar(
-    title: const Text(
-      'FireArchive🧑‍🚒',
-      style: TextStyle(
-        color: Colors.black,
-        fontSize: 25,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-    backgroundColor: Colors.white,
-    elevation: 0.0,
-    centerTitle: true,
-    leading: GestureDetector(
-      onTap: () {
-        // Handle menu icon tap
-        _scaffoldKey.currentState?.openDrawer();
-      },
-      // Handle menu
-      child: Container(
-        margin: const EdgeInsets.all(10),
-        alignment: Alignment.center,
-        child: SvgPicture.asset(
-          'assets/icons/menu-1.svg',
-          height: 35,
-          width: 35,
+  AppBar myAppBar() {
+    return AppBar(
+      title: const Text(
+        'FireArchive🧑‍🚒',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 25,
+          fontWeight: FontWeight.bold,
         ),
       ),
-    ),
-    actions: [
-      BlinkingSOSButton(isDanger: _isDanger,), // Use the BlinkingSOSButton widget here
-    ],
-  );
-}
-
+      backgroundColor: Colors.white,
+      elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          // Handle menu icon tap
+          _scaffoldKey.currentState?.openDrawer();
+        },
+        // Handle menu
+        child: Container(
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/menu-1.svg',
+            height: 35,
+            width: 35,
+          ),
+        ),
+      ),
+      actions: [
+        BlinkingSOSButton(
+          isDanger: _isDanger,
+        ), // Use the BlinkingSOSButton widget here
+      ],
+    );
+  }
 
   Widget buildBody() {
     return Column(
@@ -408,107 +393,107 @@ class MapSampleState extends State<MapSample> {
   }
 
   Widget buildSearchRow() {
-  return Container(
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(10),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.5),
-          spreadRadius: 1,
-          blurRadius: 4,
-          offset: Offset(0, 2),
-        ),
-      ],
-    ),
-    child: Row(
-      children: [
-        Expanded(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10),
-            child: TextFormField(
-              controller: _locationController,
-              decoration: InputDecoration(
-                hintText: 'Search Location',
-                hintStyle: TextStyle(
-                  color: Color(0xffDDDADA),
-                  fontSize: 15,
-                ),
-                border: InputBorder.none,
-                prefixIcon: Padding(
-                  padding: const EdgeInsets.all(8),
-                  child: SvgPicture.asset(
-                    'assets/icons/loc-1.svg',
-                    colorFilter: const ColorFilter.mode(
-                      Color.fromARGB(255, 109, 107, 106),
-                      BlendMode.srcIn,
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 4,
+            offset: Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: TextFormField(
+                controller: _locationController,
+                decoration: InputDecoration(
+                  hintText: 'Search Location',
+                  hintStyle: TextStyle(
+                    color: Color(0xffDDDADA),
+                    fontSize: 15,
+                  ),
+                  border: InputBorder.none,
+                  prefixIcon: Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: SvgPicture.asset(
+                      'assets/icons/loc-1.svg',
+                      colorFilter: const ColorFilter.mode(
+                        Color.fromARGB(255, 109, 107, 106),
+                        BlendMode.srcIn,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              onChanged: (value) {
-                searchedLocation = value;
-                searchLocation(searchedLocation);
-              },
-            ),
-          ),
-        ),
-        IconButton(
-          onPressed: () async {
-            searchLocation(searchedLocation);
-          },
-          icon: Icon(
-            Icons.search,
-            color: Color(0xffDDDADA),
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-
-  Widget buildFloatingActionButton() {
-  return Padding(
-    padding: const EdgeInsets.only(bottom: 50.0),
-    child: Tooltip(
-      message: 'Current Location', // Tooltip text
-      child: FloatingActionButton(
-        onPressed: () async {
-          getUserCurrentLocation().then((value) async {
-            markers.add(
-              Marker(
-                markerId: const MarkerId("0"),
-                position: LatLng(value.latitude, value.longitude),
-                infoWindow: const InfoWindow(
-                  title: 'My Current Location',
-                ),
-                icon: BitmapDescriptor.defaultMarkerWithHue(
-                    BitmapDescriptor.hueAzure),
-                onTap: () {
-                  _showMyDialog(value.latitude, value.longitude);
+                onChanged: (value) {
+                  searchedLocation = value;
+                  searchLocation(searchedLocation);
                 },
               ),
-            );
-
-            CameraPosition cameraPosition = CameraPosition(
-              target: LatLng(value.latitude, value.longitude),
-              zoom: 8,
-            );
-
-            _userPosition_lat = value.latitude;
-            _userPosition_lng = value.longitude;
-
-            final GoogleMapController controller = await _controller.future;
-            controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-            setState(() {});
-
-            _setSOS();
-          });
-        },
-        child: const Icon(Icons.location_on), // Icon for current location
+            ),
+          ),
+          IconButton(
+            onPressed: () async {
+              searchLocation(searchedLocation);
+            },
+            icon: Icon(
+              Icons.search,
+              color: Color(0xffDDDADA),
+            ),
+          ),
+        ],
       ),
-    ),
-  );
-}
+    );
+  }
+
+  Widget buildFloatingActionButton() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50.0),
+      child: Tooltip(
+        message: 'Current Location', // Tooltip text
+        child: FloatingActionButton(
+          onPressed: () async {
+            getUserCurrentLocation().then((value) async {
+              markers.add(
+                Marker(
+                  markerId: const MarkerId("0"),
+                  position: LatLng(value.latitude, value.longitude),
+                  infoWindow: const InfoWindow(
+                    title: 'My Current Location',
+                  ),
+                  icon: BitmapDescriptor.defaultMarkerWithHue(
+                      BitmapDescriptor.hueAzure),
+                  onTap: () {
+                    _showMyDialog(value.latitude, value.longitude);
+                  },
+                ),
+              );
+
+              CameraPosition cameraPosition = CameraPosition(
+                target: LatLng(value.latitude, value.longitude),
+                zoom: 8,
+              );
+
+              _userPosition_lat = value.latitude;
+              _userPosition_lng = value.longitude;
+
+              final GoogleMapController controller = await _controller.future;
+              controller.animateCamera(
+                  CameraUpdate.newCameraPosition(cameraPosition));
+              setState(() {});
+
+              _setSOS();
+            });
+          },
+          child: const Icon(Icons.location_on), // Icon for current location
+        ),
+      ),
+    );
+  }
 }
